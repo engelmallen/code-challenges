4fb2219740219c95a761b56cf6a7d7ec4f0dd008
Engel Mallen <emallen@uarts.edu>
2020-03-22 17:51:51 -0400

    challenge 9 done...  im not happy tho

---
+++ codCha9.js
@@ -0,0 +1,49 @@
+// I need to get rid of the "/static/" and have as a final result an array that contains
+// files only in this format => "image1.jpg"
+
+let images = 
+	["/static/image1.jpg", "/static/image2.jpg", "/static/image3.jpg", "/static/image4.jpg",
+	 "/static/image5.jpg", "/static/image6.jpg", "/static/image7.jpg", "/static/image8.jpg",
+	 "/static/image9.jpg", "/static/image10.jpg", "/static/image11.jpg", "/static/image12.jpg"
+	]
+
+/*function killStatic(thisVar)
+{
+	thisVar.forEach(function(x)
+			{
+			x = x.split("/static/")
+			x = x.shift()
+			}
+		)
+	console.log(thisVar)
+}
+
+ killStatic(images)*/
+
+// console.log(images)
+
+
+
+
+
+
+			// or
+let changer = []
+
+images.forEach( function(a)
+	{changer.push(a)} 
+	)
+
+images = []
+
+changer.forEach(function(b)
+{
+	b = b.split("/static/")
+	if(b!=0){
+		b.shift()
+		images.push(b)}
+		// console.log(b)
+}
+)
+
+console.log(images)
