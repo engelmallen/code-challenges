1cd8afe3e5cae35bfdc2b53e93dca4aaa86db9d4
Engel Mallen <emallen@uarts.edu>
2020-03-22 15:00:46 -0400

    challenge 8 update

--- codCha7.js
+++ codCha7.js
@@ -3,43 +3,39 @@
 another array all numbers bigger than 10; if there are no numbers bigger than 
 ten just console.log("no numbers bigger than 10");*/
 
-// var arOfNums = 
-// [2,5,/*32,54,1,12,87,*/6]
+		/*var arOfNums = 
+		[2,5,32,54,1,12,87,6]
 
-/*function ovTen()
-{
-	var checkNums = []
-	for (var i = 0; i <= arOfNums.length; i++) {
-		if (arOfNums[i]>9) {
-			checkNums.push(arOfNums[i])
+		var b = []
+
+		arOfNums.forEach( function(a)
+		{	
+			if (a > 9) 
+				{
+					b.push(a)
+					console.log(b)
+				}
 		}
-	}
-	if (!checkNums === 0){
-		console.log(checkNums)
-		} else {
-				console.log("no numbers bigger than 10")
-			}
-}*/
-
-// ovTen()
+		)
+		if (b==0) {console.log("no numbers bigger than 10")}*/
 
 /*4. Write a function that takes a string as an argument and returns (console.logs) 
 all the vowels in this string*/
 
-/*function allVows (x)
-{
-	// x = "hello";
-	x.split("");
-	var vowels = [];
-	for (var i = 0; i <=  x.length; i ++) 
-		{
-		if (x[i] == "a" || x[i] == "e" || x[i] == "i"  || x[i] == "o"  || x[i] == "u")
+			/*function allVows (x)
 			{
-				vowels.push(x[i])
+				// x = "hello";
+				x.split("");
+				var vowels = [];
+				for (var i = 0; i <=  x.length; i ++) 
+					{
+					if (x[i] == "a" || x[i] == "e" || x[i] == "i"  || x[i] == "o"  || x[i] == "u")
+						{
+							vowels.push(x[i])
+						}
+				} console.log(vowels.join())
 			}
-	} console.log(vowels)
-}*/
-
+			*/
 
 
 
@@ -47,29 +43,22 @@ all the vowels in this string*/
 lists all the words that are longer than 5 characters in this sentence. 
 IF there is no word longer than five characters console.log('very short words")*/
 
-/*
-function who(sentence)
-{
-	sentence = sentence.split(" ")
-	var words = [];
+			/*var five = []
 
-	for(i = 0; i <= sentence.length; i ++) 
-		{	
-			if 	(sentence[i].length>=5)
-				{
-				words.push(sentence[i])
-				}
-		}
+			function who(sentence)
+			{
+				sentence = sentence.split(" ")
+				words = []
+				sentence.forEach(function(a)
+					{
+						if (a.length > 4) {words.push(a)}
+					} 
+				) 
+			if (words == 0) {console.log("very short words")}
+				else {console.log(words)}
+			}
 
-	// if (words==0) 
-	// 	{
-	// 		console.log('very short words')
-	// 	}
-	// 	else 	
-	// 		{
-				console.log(words)
-			// }		
-}*/
+			who("alla donde soliamos gritar")*/
 
 
 
@@ -98,39 +87,97 @@ if a number is divisible by both 3 and 5 console.log("FizzBuzz")*/
 the first word with “ish” added to the end. Example:
 ishFunc(“Soup is my favorite.“) -> “Soupish”*/
 
-/*function addIsh(toIsh)
-{
-	toIsh = toIsh.split(" ");
-	console.log(toIsh[0]+"ish");
-}
-*/
+			/*function addIsh(toIsh)
+			{
+				toIsh = toIsh.split(" ");
+				console.log(toIsh[0]+"ish");
+			}
+
+			addIsh("The Demon and Mrs. Prynn")*/
+
 
 
 
 /*8. Write a function that takes and array of numbers and adds the sum of them.*/
 
-/*function arNums([a,b,c,d,e,f,g])
-{
-	console.log(a+b+c+d+e+f+g)
-}*/
-			//or
-
-arOfNums([6,7,8,9,11,13,23,34,111,234])
-
-function arAdd(arNum)
-{
-	var sum = 0;
-	for (var i = 0; i <= arNum.length; i++) 
+ 		/*function arNums([a,b,c,d,e,f,g])
 		{
-			sum += arNum[i]; 
-		} 
-	console.log(sum)
-}
+			console.log(a+b+c+d+e+f+g)
+		}arNums([3,4,23,4,6,2,8])
+
+			//or*/
+
+		/*var arOfNums = [6,7,8,9,11,13,23,34,111,234]
+
+		function arAdd()
+		{
+			var sum = 0;
+			for (var i = 0; i < arOfNums.length; i++) 
+				{
+					sum += arOfNums[i]; 
+					
+				}console.log(sum)
+		}arAdd()*/
 
 
 
-// 9. Write a function that takes a string argument and returns all the instances of vowels ex. -> “whatever” - returns “aee”
-// 10. Create an array. Using the .forEach() method on the array, print each element to  the console. If you are unsure on how to use .forEach(), Google it.
-// 11 Create a function that takes an array. Check to see if it contains a 5 or 6. If it does, return true, otherwise return false. Example:
-// checkNums( [1, 3, 5, 12, 22] ) -> true
-//checkNums( [30, 99, 8, 22, 7] ) -> false
+/*9. Write a function that takes a string argument and returns all 
+the instances of vowels ex. -> “whatever” - returns “aee”*/
+
+		/*function allVows (x)
+		{
+			// x = "hello";
+			x.split("");
+			var vowels = "";
+			for (var i = 0; i <=  x.length; i ++) 
+				{
+				if (x[i] == "a" || x[i] == "e" || x[i] == "i"  || x[i] == "o"  || x[i] == "u")
+					{
+						vowels += x[i];
+					}
+			// vowels.join();
+			} console.log(vowels)
+		}allVows ("las palabras fueron avispas y las calles como dunas")*/
+
+
+/*10. Create an array. Using the .forEach() method on the array, 
+print each element to  the console. If you are unsure on how to use .forEach(), 
+Google it.*/
+ 
+			/*var groceries = 
+					 [
+					 	"eggs",
+					 	"Toilet Paper",
+					 	"Bread",
+					 	"Water",
+					 	"milk",
+					 ]
+
+					groceries.forEach(function (a)
+					{
+						console.log("there is no " + a + " In the supermarket")
+					})
+			*/
+
+
+/*11 Create a function that takes an array. Check to see if it contains a 5 or 6.
+ If it does, return true, otherwise return false. Example:
+checkNums( [1, 3, 5, 12, 22] ) -> true
+checkNums( [30, 99, 8, 22, 7] ) -> false*/
+
+		// var array65 = [2,7,9,11,13,/*6,5,*/34,111,234]
+
+		// var checkVar = false;
+
+		// array65.forEach(function (x)
+		// 	{
+				
+		// 	 	if (x === 6 || x === 5)
+		// 		 	{
+		// 				checkVar = true
+		// 		 	}
+		// 	}
+		// 	)
+
+		// console.log(checkVar)
+
---
+++ codCha8.js
@@ -0,0 +1,51 @@
+/*1. Coding challenges for today:
+Write a function that takes the array below as a parameter and lists all the names in it:*/
+
+		var names = 
+		[
+		    {name: "OGGI"},
+		    {name: "GEORGE"},
+		    {name: "JOHN"},
+		    {name: "JAMES"},
+		    {name: "PAUL"},
+		    {name: "JEREMY"}
+		]
+
+		/*function allNames()
+		{
+			for(i=0; i < names.length; i++)
+				{
+					console.log(names[i].name)
+				}
+		}
+		allNames()*/
+							//or
+
+/*names.foreach( function(a)
+{
+	console.log(a)
+}
+
+
+	)
+*/
+
+
+/*2. Write a function that lists all the numbers from 100 to 0; 
+if a number is divisible by 4 print “This is divisible by 4”; 
+if a number is divisible by 6 prints “THis is divisible by 6"; 
+if a number is divisible by both 4 and 6 print “This one is divisible by both 4 and 6”.*/
+
+		/*function oneToHundred()
+		{
+		for(i = 100; i >= 0; i --)
+			{
+				if (i%4==0 && i%6==0) {console.log(i+" This is divisible by 4 and 6")}
+				else if (i%4==0) {console.log(i+" This is divisible by 4")}
+				else if (i%6==0) {console.log(i+" This is divisible by 6")}
+				else {console.log(i)}
+			}
+		}
+		oneToHundred()*/
+
+								// or
